1c04c825cf712b267e710610c1b36304feffee1f
Jake Bolam <jake.bolam@gmail.com>
2018-04-29 20:26:12 -0400

    Convert project to nodejs

--- scripts/stop_local_dynamo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-# get_physical_resource_id ${stack-name} ${logical-resource-id}
-get_docker_container_id() {
-    echo $(docker ps -a -q --filter ancestor=$1 --format="{{.ID}}")
-}
-
-DYNAMO_CONTAINERS=$(get_docker_container_id "tray/dynamodb-local")
-docker stop ${DYNAMO_CONTAINERS} >> /dev/null
